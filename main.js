e1750d8700b75192b93763eafa13f88e9b7ba404
AGCAL1508 <AGcaliwag1508@gmail.com>
2022-05-09 19:09:05 +0800

    made the icons smaller;changed the positioning of certain elements and fixed width of main content

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-const { app, BrowserWindow } = require('electron')
-
-app.whenReady().then(
-  function () {
+const { app, BrowserWindow } = require('electron');
+const path = require('path')
+function createwindow(){
     let Main_Window = new BrowserWindow({
-      width: 1000,
-      height: 700
+        minWidth: 1000,
+        minHeight: 700,
+        width: 1000,
+        height: 700,
+        webPreferences: {
+            nodeIntegration: true,
+            preload: path.join(__dirname, 'preload.js')
+        },
     });
     Main_Window.loadFile('index.html');
     Main_Window.removeMenu();
-  }
-)
+};
+function run_app() {
+    createwindow()
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createwindow()
+    });
+}
+app.whenReady().then(run_app)
+app.on('window-all-closed', () => {
+    if (process.platform !== 'drawin') app.quit();
+});
