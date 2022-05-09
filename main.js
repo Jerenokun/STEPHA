569b8a35d66b07fcc0c866a60844ab3bebfc186d
AGCAL1508 <AGcaliwag1508@gmail.com>
2022-05-09 11:08:23 +0800

    added rounded edges to the to do list icon and added some padding

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const { app, BrowserWindow } = require('electron')
 app.whenReady().then(
   function () {
     let Main_Window = new BrowserWindow({
-      width: 500,
-      height: 500
+      width: 1000,
+      height: 700
     });
     Main_Window.loadFile('index.html');
     Main_Window.removeMenu();
