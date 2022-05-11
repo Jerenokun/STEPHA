a5001b7047aa5018b681ca467dc42120ecefbd77
AGCAL1508 <AGcaliwag1508@gmail.com>
2022-05-11 23:02:50 +0800

    added comments to make everything organized

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path')
+// creating the window
 function createwindow(){
     let Main_Window = new BrowserWindow({
         minWidth: 1000,
@@ -11,16 +12,24 @@ function createwindow(){
             preload: path.join(__dirname, 'preload.js')
         },
     });
+// loading the html
     Main_Window.loadFile('index.html');
+// Removing the menubar. The menubar is basically the "File|Edit|View... parts that you see in something like the task manager"
     Main_Window.removeMenu();
 };
+// Runnning the app
 function run_app() {
     createwindow()
+// Minimizing the amount of windows open to one I think
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createwindow()
     });
 }
+
+// When the app is ready and everything is good, then it will run the app
 app.whenReady().then(run_app)
+
+// Basically quitting the application once all the windows are closed.
 app.on('window-all-closed', () => {
-    if (process.platform !== 'drawin') app.quit();
+    if (process.platform !== 'darwin') app.quit();
 });
