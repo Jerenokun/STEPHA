11e92394f031574f925d586b81e55ea3b6117a0d
AGCAL1508 <AGcaliwag1508@gmail.com>
2022-05-12 13:22:24 +0800

    added themesbutton, settings button and time

--- index.js
+++ index.js
@@ -2,16 +2,38 @@ let allpages = ['homepage','todolist_page']
 function todolist_page() {
     for (let i = 0; i < allpages.length;i++) {
         let page = document.getElementById(allpages[i]);
-        page.style.display = "none";
+        page.style.display = 'none';
     }
-    let page = document.getElementById("todolist_page");
-    page.style.display = "flex"
+    let page = document.getElementById('todolist_page');
+    page.style.display = 'flex'
 }
 function home() {
     for (let i = 0; i < allpages.length;i++) {
         let page = document.getElementById(allpages[i]);
-        page.style.display = "none"
+        page.style.display = 'none'
     }
-    let page = document.getElementById("homepage");
-    page.style.display = "grid"
+    let page = document.getElementById('homepage');
+    page.style.display = 'grid'
+}
+function changetheme() {
+    
+}
+window.onload = function () {
+    let currenttime = new Date()
+    let timetag = document.getElementById('time')
+    let hours = currenttime.getHours()
+    if (hours > 11) {
+        var meridiem = 'pm'
+        if (hours > 12) {
+            hours -= 12
+        }
+    }
+    timetag.textContent = `${hours}:${currenttime.getMinutes()} ${meridiem}`
+    setInterval(() => {
+        currenttime = new Date()
+        hours = currenttime.getHours()
+        timetag.textContent = `${hours}:${currenttime.getMinutes()} ${meridiem}`
+        console.log('yes')
+    }, 5000);
+
 }
--- main.js
+++ main.js
@@ -2,30 +2,28 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path')
 // creating the window
 function createwindow(){
-    let Main_Window = new BrowserWindow({
+    let mainwin = new BrowserWindow({
         minWidth: 1000,
         minHeight: 700,
         width: 1000,
         height: 700,
         webPreferences: {
             nodeIntegration: true,
-            preload: path.join(__dirname, 'preload.js')
         },
     });
 // loading the html
-    Main_Window.loadFile('index.html');
+    mainwin.loadFile('index.html');
 // Removing the menubar. The menubar is basically the "File|Edit|View... parts that you see in something like the task manager"
-    Main_Window.removeMenu();
+    mainwin.setMenu(null);
 };
 // Runnning the app
 function run_app() {
     createwindow()
-// Minimizing the amount of windows open to one I think
+// For MACOS: To avoid any problems in creating windows when the dock icon is clicked
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createwindow()
     });
 }
-
 // When the app is ready and everything is good, then it will run the app
 app.whenReady().then(run_app)
 
