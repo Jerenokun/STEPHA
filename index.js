c8c6ac51d17fe76052274f7aecef323d21ef2f5b
AGCAL1508 <AGcaliwag1508@gmail.com>
2022-05-13 21:54:19 +0800

    added checker for valid inputs for adding task

--- index.js
+++ index.js
@@ -11,23 +11,74 @@ function show_page(p) {
     page.style.zIndex = 3;
     page.style.opacity = 1;
 }
+function check_task() {
+    let elements = ["task_name", "task_info", "task_date", "task_time"];
+    let AnswersNotBlank = true;
+    for (let i = 0; i < elements.length; i++) {
+        if (
+            document.getElementById(elements[i]).value.split(" ").join("") == ""
+        ) {
+            AnswersNotBlank = false;
+        }
+    }
+    if (AnswersNotBlank) {
+        let inputdate = document.getElementById("task_date").value;
+        let inputtime = document.getElementById("task_time").value;
+        datetime = new Date(`${inputdate} ${inputtime}`);
+        currentdate = new Date();
+        currenttime = currentdate;
+        currentdate = `${currentdate.getFullYear()}-${String(
+            currentdate.getMonth() + 1
+        ).padStart(2, "0")}-${String(currentdate.getDate()).padStart(2, "0")}`;
+        if (inputdate == currentdate) {
+            currenttime = `${String(currenttime.getHours()).padStart(
+                2,
+                "0"
+            )}:${String(currenttime.getMinutes()).padStart(2, "0")}`;
+            if (inputtime > currenttime) {
+                console.log("sucess");
+            } else {
+                alert(
+                    "Please enter a time in the future, not in the past! [Keep In mind, this is military time]"
+                );
+            }
+        } else {
+            alert("Please enter today's date or a date in the future!");
+        }
+    } else {
+        alert("Please fill up all the fields!");
+        return false;
+    }
+}
+
+function add_task() {
+    console.log(check_task());
+}
 function AssignActionToButton() {
     document.getElementById("home_button").addEventListener("click", () => {
         hide_pages();
         show_page("homepage");
     });
-    document.getElementById("things_to_do_button").addEventListener("click", () => {
-        hide_pages();
-        show_page("todolist_page");
-    });
-    document.getElementById('daily_reminders_button').addEventListener("click", () => {
-        hide_pages();
-        show_page("reminders_page");
-    });
-    document.getElementById("themesbutton").addEventListener('click', () => {
+    document
+        .getElementById("things_to_do_button")
+        .addEventListener("click", () => {
+            hide_pages();
+            show_page("todolist_page");
+        });
+    document
+        .getElementById("daily_reminders_button")
+        .addEventListener("click", () => {
+            hide_pages();
+            show_page("reminders_page");
+        });
+    document.getElementById("themesbutton").addEventListener("click", () => {
         let colors_list = document.querySelector(":root");
+        let themesbutton = document.getElementById("themesbutton");
+        themesbutton.style.backgroundImage =
+            "url(../assets/darkmodebutton.svg)";
         colors_list.classList.toggle("dark_mode");
     });
+    document.getElementById("add_task").addEventListener("click", add_task);
 }
 
 function ChangeGreetingText() {
